9c8f62dce7e5c2fc5b1ff3bdf17084d5909d7e34
misanc21 <misanc21@gmail.com>
2020-09-06 15:33:06 -0500

    buscar noticias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React,  {Fragment, useState} from 'react';
+import React,  {Fragment, useState, useEffect} from 'react';
+import axios from 'axios'
+
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
 function App() {
 
   const [categoria, setCategoria] = useState('')
+  const [noticias, setNoticias] = useState ([])
+
+  useEffect(()=>{
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=b894c24ec9f746c2b5f0a2ab455b6931`
+      const respuesta = await axios.get(url)
+      setNoticias(respuesta.data.articles)
+    }
+    consultarApi()
+  },[categoria])
 
   return (
     <Fragment>
